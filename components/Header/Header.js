1303cd085b13c6911af93ec26795a7aee442224f
Thilak Reddy <thilakreddy19102000@gmail.com>
2022-09-25 21:51:36 +0530

    Correcting google analytics id

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,43 +1,43 @@
 import Head from "next/head";
-const Header=()=>{
-    return (
+const Header = () => {
+  return (
     <Head>
-        <title>JNTUH B.Tech Results</title>
-        <link id="favicondark" rel='icon' href='/favicon-black.png' />
-        <meta
-          property='og:url'
-          content='https://jntuhresults.vercel.app/'
-        />
-        <meta property='og:type' content='website' />
-        <meta property='og:title' content='JNTUH B.Tech Results' />
-        <meta name='twitter:card' content='summary' />
-        {/* <meta name="viewport" content="width=1024"></meta> */}
-        <meta
-          property='og:description'
-          content='You can find the Results of your entire B Tech semesters here of Jawaharlal Nehru Technological University, Hyderabad (JNTUH).'
-        />
-        <meta
-          property='og:image'
-          content={
-            'https://user-images.githubusercontent.com/64121161/168486734-5d799aed-6110-47aa-8338-a0aa3c70e963.png'
-          }
-        />
-        <Script
-        async src="https://www.googletagmanager.com/gtag/js?id=G-6CR1W425NE"
+      <title>JNTUH B.Tech Results</title>
+      <link id="favicondark" rel='icon' href='/favicon-black.png' />
+      <meta
+        property='og:url'
+        content='https://jntuhresults.vercel.app/'
+      />
+      <meta property='og:type' content='website' />
+      <meta property='og:title' content='JNTUH B.Tech Results' />
+      <meta name='twitter:card' content='summary' />
+      {/* <meta name="viewport" content="width=1024"></meta> */}
+      <meta
+        property='og:description'
+        content='You can find the Results of your entire B Tech semesters here of Jawaharlal Nehru Technological University, Hyderabad (JNTUH).'
+      />
+      <meta
+        property='og:image'
+        content={
+          'https://user-images.githubusercontent.com/64121161/168486734-5d799aed-6110-47aa-8338-a0aa3c70e963.png'
+        }
+      />
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-6CR1W425NE"
         strategy="afterInteractive"
       />
       <Script id="google-analytics" strategy="afterInteractive">
         {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
+        window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
 
-          gtag('config', 'GA_MEASUREMENT_ID');
+          gtag('config', 'G-6CR1W425NE');
         `}
       </Script>
-        <script
-            dangerouslySetInnerHTML={{
-              __html: `
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
               if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
                     console.log("dark Mode");         
                 }
@@ -51,10 +51,10 @@ const Header=()=>{
                   window.location.reload();
                  };
                   `,
-            }}
-          >    
-        </script>
+        }}
+      >
+      </script>
     </Head>
-    )
+  )
 }
 export default Header;
