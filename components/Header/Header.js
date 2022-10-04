0e5375f1e5c31354e969ff8fa62bde81b7b1bee3
Thilak Reddy <thilakreddy19102000@gmail.com>
2022-10-04 21:54:18 +0530

    Add:Favicon

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -5,7 +5,10 @@ const Header = () => {
     <>
       <Head>
         <title>JNTUH B.Tech Results</title>
-        <link id="favicondark" rel='icon' href='/favicon-black.png' />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
         <meta
           property='og:url'
           content='https://jntuhresults.vercel.app/'
