36c41bfdcc87c094b610d8e73e68826d9d934157
s vamshi <somsolevamshi@gmail.com>
2022-11-09 14:57:39 +0530

    print btn and scroll to top added

--- components/Single/SingleResults.js
+++ components/Single/SingleResults.js
@@ -1,24 +1,17 @@
 
-import React, { useState } from 'react';
+import React from 'react';
+import PrintButton from '../ui/PrintButton';
+import ScrollToTop from "react-scroll-to-top";
 
 const SingleResults = ({ query }) => {
   const Results = query['Results'];
   const Details = query['Details'];
-  const print = () => {
-    setDisplay("hidden")
-    setTimeout(function () {
-      window.print();
-    }, 500);
-  }
-  const [display, setDisplay] = useState("fixed bottom-8 right-6 sm:right-8 sm:bottom-8 bg-white rounded-full");
 
   return (
     <>
-      {/* <div id="printi" className={display}>
-        <img src='./download.png' onClick={print} className="w-[40px]  sm:w-[60px]" />
-      </div> */}
+      <PrintButton />
       <div className="m-2 text-[45%] sm:text-[60%] md:text-[80%] lg:text-[100%]">
-        {Object.keys(Results).map((val, ind) => {
+        {Object.keys(Results).map((val) => {
           if (val != 'Total') {
             return <>
               <div id='1'>
@@ -81,6 +74,14 @@ const SingleResults = ({ query }) => {
           </table>
         </div>
       </div>
+      <PrintButton />
+      <ScrollToTop
+        className='scroller'
+        smooth
+        viewBox="-5 0 18 18"
+        svgPath="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"
+        style={{bottom:"30px", opacity:0.75, backgroundColor:'grey'}}
+      />
     </>
   )
 }
---
+++ components/ui/PrintButton.js
@@ -0,0 +1,22 @@
+
+import React, { useState } from 'react';
+
+const PrintButton = () => {
+  const print = () => {
+    setTimeout(function () {
+      window.print();
+    }, 500);
+  }
+
+  return(
+    <>
+      <div className="printi flex flex-col items-center invisible md:visible">
+        <button onClick={print} className="bg-blue-500 text-white font-bold rounded p-2 my-2">Print</button>
+      </div>
+    </>
+  )
+}
+export default PrintButton
+
+
+
