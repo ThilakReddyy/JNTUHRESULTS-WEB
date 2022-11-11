b0e973f6582c5f463bd8e1b4d639a519078baa5c
s vamshi <somsolevamshi@gmail.com>
2022-11-11 06:04:26 +0530

    fix: spacing issue

--- components/ui/PrintButton.js
+++ components/ui/PrintButton.js
@@ -10,7 +10,7 @@ const PrintButton = () => {
 
   return(
     <>
-      <div className="printi flex flex-col items-center invisible md:visible">
+    <div className="flex-col items-center hidden md:flex">
         <button onClick={print} className="bg-blue-500 text-white font-bold rounded md:p-2 md:my-2">Print</button>
       </div>
     </>
