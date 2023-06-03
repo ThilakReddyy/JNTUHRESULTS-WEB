e7734fa812412d61885014b8a59e66e8b414c177
THILAK REDDY <64121161+ThilakReddyy@users.noreply.github.com>
2023-06-04 03:04:53 +0530

    Update Notificationscraper.js

--- components/Notifications/Notificationscraper.js
+++ components/Notifications/Notificationscraper.js
@@ -55,31 +55,7 @@ const NotificationScraper = () => {
 
                         </div>
                     </div>
-                    <div className="bg-gray-100  bg-gradient-to-br from-indigo-50 via-white to-cyan-100 pt-[75px]">
-                        <div className="container mx-auto py-8 px-4">
-                            <h1 className="text-3xl font-semibold text-center mb-8">Notifications</h1>
-
-                            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-                                {results.map((result, index) => (
-                                    <div
-                                        key={index}
-                                        className="bg-white p-6 rounded-lg shadow-lg "
-                                    >
-                                        <h2 className="text-lg font-bold mb-4">{result.Result_title}</h2>
-                                        <p className="text-gray-600 mb-4">Date: {result.Date}</p>
-                                        <a
-                                            href={result.Link}
-                                            className="text-blue-500 hover:text-blue-700 underline"
-                                            target="_blank"
-                                            rel="noopener noreferrer"
-                                        >
-                                            View Results
-                                        </a>
-                                    </div>
-                                ))}
-                            </div>
-                        </div>
-                    </div>
+                   
                 </center>
             )}
         </div>
