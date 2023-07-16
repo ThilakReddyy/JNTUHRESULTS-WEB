fd0e427c95019707c660f6a03caa744cd102df32
Thilak Reddy <thilakreddy19102000@gmail.com>
2023-07-16 10:05:11 +0530

    Fix:Notification

--- components/ClassResult/ClassResultPage.jsx
+++ components/ClassResult/ClassResultPage.jsx
@@ -47,7 +47,7 @@ const AcademicReportPage = () => {
         var currentHour = currentTime.getHours();
         if (currentHour < 7) {
 
-            setWarning("Result not available at this time");
+            setWarning("");
         }
         else {
             setWarning("Results are only available from 12 AM to 7 AM")
@@ -62,7 +62,7 @@ const AcademicReportPage = () => {
         var currentTime = new Date();
         var currentHour = currentTime.getHours();
 
-        if (currentHour >= 7 || currentHour < 7) {
+        if (currentHour >= 7) {
             console.log("Result not available at this time")
             return "";
         }
