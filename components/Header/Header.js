f39cbaa8d902b4579672e8abc5046d481b83c865
Thilak Reddy <thilakreddy19102000@gmail.com>
2023-07-30 06:18:37 +0530

    add:pwa

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -21,7 +21,7 @@ const Header = () => {
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="manifest" href="/manifest.json" />
         <link rel="canonical" href="https://jntuhresults.vercel.app" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" alt="Favicon 32x32" />
