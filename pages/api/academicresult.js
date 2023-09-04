efe9928187f682dc4d413123b41fad0e23c0536d
Thilak Reddy <thilakreddy19102000@gmail.com>
2023-09-04 15:44:30 +0530

    add:academicresult

--- pages/api/academicresult.js
+++ pages/api/academicresult.js
@@ -267,8 +267,14 @@ class ResultScraper {
 
 
 export default async function handler(req, res) {
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+
+
     // Usage
-    console.log()
     const startTime = performance.now();
     const rollNumber = req.query['htno'];
     const jsonString = await redis.get(rollNumber);
