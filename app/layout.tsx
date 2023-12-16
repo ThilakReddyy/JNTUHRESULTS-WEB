53d80eeb8ce0ae9b8d19bc5477cf3dc031b877e0
thilakreddyy <thilakreddy19102000@gmail.com>
2023-12-16 20:26:17 +0530

    Fix toggle issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="light"
           enableSystem
           disableTransitionOnChange
         >
