aa2e8d91655ebcfd009d80adcb05e2adb819230b
thilakreddyy <thilakreddy19102000@gmail.com>
2023-12-18 14:38:57 +0530

    add:results

--- app/academicresult/result/page.tsx
+++ app/academicresult/result/page.tsx
@@ -1,25 +1,56 @@
 "use client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSearchParams, useRouter } from "next/navigation";
 import { getLocalStoragedata } from "@/components/api/fetchAcademicResult";
+import { calculateResult } from "@/components/customfunctions/calculateresult";
 
 const AcademicResultResult = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
   const htno = searchParams.get("htno");
+  const [renderKey, setRenderKey] = useState(0);
   const details = getLocalStoragedata(String(htno));
-  let Details = {};
-  let Results = {};
+  const grades = ["O", "A+", "A", "B+", "B", "C", "F", "P", "Ab", "-"];
+  const credits: number[] = [];
+  const { Details: resultdetails, Results: resultresults } =
+    details?.value || {};
+  const [Details, setDetails] = useState(resultdetails);
+  const [Results, setResults] = useState(resultresults);
+  for (let i = 0; i < 15; i += 0.5) {
+    credits.push(i);
+  }
+  useEffect(() => {
+    setResults(calculateResult(Results));
+    setRenderKey((prevkey) => prevkey + 1);
+    console.log(Results);
+  }, [Results]);
+  function editResults(
+    value: string,
+    subject_key: string,
+    newValue: string,
+    keychange: string,
+  ) {
+    setResults((prevResults: typeof Results) => ({
+      ...prevResults,
+      [value]: {
+        ...prevResults[value],
+        [subject_key]: {
+          ...prevResults[value][subject_key],
+          [keychange]: newValue,
+        },
+      },
+    }));
+
+    console.log(value, subject_key, newValue);
+  }
+
+  const [edit, setEdit] = useState(false);
   useEffect(() => {
     if (details === null) {
       router.push("/academicresult");
     }
   }, [details, router]);
-  if (details !== null) {
-    const resultDetails = details.value;
-    Details = resultDetails["Details"];
-    Results = resultDetails["Results"];
-  }
+
   return details === null ? (
     <>
       <div className="m-2 text-[30%] sm:text-[45%] md:text-[60%] lg:text-[100%]">
@@ -27,9 +58,21 @@ const AcademicResultResult = () => {
       </div>
     </>
   ) : (
-    <div className="m-2 text-[30%] sm:text-[45%]  md:text-[60%] lg:text-[100%]">
-      <div className="text-center font-bold my-5 text-xs lg:text-2xl">
-        ACADEMIC RESULT
+    <div
+      key={renderKey}
+      className="m-2 text-[30%]  sm:text-[45%]  md:text-[60%] lg:text-[100%]"
+    >
+      <div className="text-center grid grid-cols-3 font-bold my-5 text-xs lg:text-2xl">
+        <div></div>
+        <div className="justify-center">ACADEMIC RESULT</div>
+        <div className="justify-end flex ">
+          <button
+            className="text-xs items-center px-2 border rounded text-white  bg-blue-600 "
+            onClick={() => setEdit(!edit)}
+          >
+            {edit ? "save" : "edit"}
+          </button>
+        </div>
       </div>
       <table className="w-[100%] my-2  border-black dark:border-white   ">
         <tbody>
@@ -74,32 +117,83 @@ const AcademicResultResult = () => {
                       <th className="dark:border-white">GRADE</th>
                       <th className="dark:border-white">CREDITS</th>
                     </tr>
-                    {Object.values(Results[value as keyof typeof Results]).map(
-                      (value: any, index: number) => {
-                        if (typeof value === "object") {
+                    {Object.values(Results[value]).map(
+                      (subjectvalue: any, index: number) => {
+                        if (typeof subjectvalue === "object") {
                           return (
                             <tr key={index}>
                               <th className="dark:border-white">
-                                {value["subject_code"]}
+                                {subjectvalue["subject_code"]}
                               </th>
                               <th className="dark:border-white">
-                                {value["subject_name"]}
+                                {subjectvalue["subject_name"]}
                               </th>
                               <th className="dark:border-white">
-                                {value["subject_internal"]}
+                                {subjectvalue["subject_internal"]}
                               </th>
                               <th className="dark:border-white">
-                                {value["subject_external"]}
+                                {subjectvalue["subject_external"]}
                               </th>
                               <th className="dark:border-white">
-                                {value["subject_total"]}
+                                {subjectvalue["subject_total"]}
                               </th>
-                              <th className="dark:border-white">
-                                {value["subject_grade"]}
-                              </th>
-                              <th className="dark:border-white">
-                                {value["subject_credits"]}
-                              </th>
+                              {edit ? (
+                                <th className="dark:border-white">
+                                  <select
+                                    className="selectth"
+                                    value={subjectvalue["subject_grade"]}
+                                    onChange={(e) => {
+                                      const subject_key =
+                                        subjectvalue["subject_code"];
+                                      editResults(
+                                        value,
+                                        subject_key,
+                                        e.target.value,
+                                        "subject_grade",
+                                      );
+                                      console.log(e);
+                                    }}
+                                  >
+                                    {grades.map((grade) => (
+                                      <option key={grade} value={grade}>
+                                        {grade}
+                                      </option>
+                                    ))}
+                                  </select>
+                                </th>
+                              ) : (
+                                <th className="dark:border-white">
+                                  {subjectvalue["subject_grade"]}
+                                </th>
+                              )}
+                              {edit ? (
+                                <th className="dark:border-white">
+                                  <select
+                                    className="selectth"
+                                    value={subjectvalue["subject_credits"]}
+                                    onChange={(e) => {
+                                      const subject_key =
+                                        subjectvalue["subject_code"];
+                                      editResults(
+                                        value,
+                                        subject_key,
+                                        e.target.value,
+                                        "subject_credits",
+                                      );
+                                    }}
+                                  >
+                                    {credits.map((grade) => (
+                                      <option key={grade} value={grade}>
+                                        {grade}
+                                      </option>
+                                    ))}
+                                  </select>
+                                </th>
+                              ) : (
+                                <th className="dark:border-white">
+                                  {subjectvalue["subject_credits"]}
+                                </th>
+                              )}
                             </tr>
                           );
                         }
@@ -112,7 +206,7 @@ const AcademicResultResult = () => {
                     <tr>
                       <th className="dark:border-white w-[75%]">CGPA</th>
                       <th className="dark:border-white">
-                        {Results[value as keyof typeof Results]["CGPA"]}
+                        {Results[value]["CGPA"]}
                       </th>
                     </tr>
                   </tbody>
@@ -124,9 +218,7 @@ const AcademicResultResult = () => {
                 <tbody>
                   <tr>
                     <th className="dark:border-white w-[75%]">TOTAL CGPA</th>
-                    <th className="dark:border-white">
-                      {Results[value as keyof typeof Results]}
-                    </th>
+                    <th className="dark:border-white">{Results[value]}</th>
                   </tr>
                 </tbody>
               </table>
