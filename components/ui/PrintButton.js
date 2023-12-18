195635f629a564a9d8115dc1f0986a9246336b22
thilakreddyy <thilakreddy19102000@gmail.com>
2023-12-18 16:30:30 +0530

    change:printbutton:

--- components/ui/PrintButton.js
+++ components/ui/PrintButton.js
@@ -1,22 +1,22 @@
-
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const PrintButton = () => {
   const print = () => {
     setTimeout(function () {
       window.print();
     }, 500);
-  }
-
-  return(
+  };
+  return (
     <>
-    <div className="flex-col items-center hidden md:flex">
-        <button onClick={print} className="bg-blue-500 text-white font-bold rounded md:p-2 md:my-2">Print</button>
+      <div className="flex-col items-center hidden md:flex">
+        <button
+          onClick={print}
+          className="bg-blue-500 text-white font-bold rounded md:p-2 md:my-2"
+        >
+          Print
+        </button>
       </div>
     </>
-  )
-}
-export default PrintButton
-
-
-
+  );
+};
+export default PrintButton;
