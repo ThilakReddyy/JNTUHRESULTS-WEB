699aeefd556405fa18d20d7ea2e981d853a4c91b
thilakreddyy <thilakreddy19102000@gmail.com>
2024-01-10 16:23:18 +0530

    Add:Help center

---
+++ app/faq/page.tsx
@@ -0,0 +1,152 @@
+"use client";
+import { useState } from "react";
+import { AiOutlineDownCircle, AiOutlineUpCircle } from "react-icons/ai";
+import { FaGithub, FaInstagram, FaTwitter } from "react-icons/fa";
+
+const Faq = () => {
+  const faqs = [
+    {
+      question: "How does this website work?",
+      answer:
+        "When a user enters their roll number, the website asynchronously sends requests to the JNTUH server, retrieving the results for all regular and supplementary exams across all semesters associated with the given roll number. Using BeautifulSoup, the backend parses the HTML content of the responses to extract the relevant result information. An algorithm then combines the results and calculates the Cumulative Grade Point Average (CGPA) for the student. This entire process is optimized to be completed in less than 2 seconds, allowing the JNTUHRESULTS-vercel website to swiftly provide comprehensive results to the user.",
+    },
+    {
+      question: "Was the entire code written by you?",
+      answer:
+        "The entire backend logic has been developed by me. You are welcome to use the code in any way you see fit by following the GPT license, and if you have any questions or need assistance, please dont hesitate to reach out to us.",
+    },
+    {
+      question: "How do I access my grades for all semesters?",
+      answer:
+        'To access your grades for all semesters, enter your roll number in the provided inbox in the academic result page and click on the "Results" button. The results of all your semesters will be displayed.',
+    },
+    {
+      question: "What is the purpose of the backend in this project?",
+      answer:
+        "The backend serves as the backbone of the JNTUHRESULTS-vercel project. It is responsible for fetching and parsing the results of both individual students and multiple classmates. By making requests to the JNTUH website and utilizing BeautifulSoup, the backend extracts the required data and provides it to the frontend.",
+    },
+    {
+      question:
+        "How does the backend fetch the results from the JNTUH website?",
+      answer:
+        "Since the JNTUH website does not provide an API or authentication for result requests, the backend sends requests to the website and captures the responses. It then uses BeautifulSoup, a Python library for parsing HTML, to extract the relevant result information from the HTML response.",
+    },
+    {
+      question: "What technologies are used to develop the website?",
+      answer:
+        "The website is built using Next.js. Next.js enables the creation of React-based web applications with server-side rendering and the generation of static websites. The website is hosted on Vercel, which provides fast deployments served from the edge.",
+    },
+    {
+      question: "How can I report a bug or ask for help?",
+      answer:
+        "If you encounter any issues or have questions regarding the website, you can contact the developer via email thilakreddypothuganti@gmail.com or fill the issue in the Google Forms. Additionally, if you find a bug, you can submit an issue on the project's GitHub repository. You're also welcome to contribute by submitting pull requests with bug fixes or changes to the develop branch.",
+    },
+    {
+      question: "What are the available API endpoints for fetching results?",
+      answer:
+        "There are two API endpoints provided by the backend:\n\n/api/academicresult?htno=Roll_no: Fetches results for a single student using their roll number.\n\n/api/classresult?htnos=multiple_htnos_seperate_by_commas&semester=semester_code: Fetches results for multiple students within a given range of roll numbers and semester code.",
+    },
+    {
+      question: "Can I suggest ideas for new website tools or features?",
+      answer:
+        "Yes, you can message me with your ideas for small website tools that you can't find online or any other features you'd like to see.",
+    },
+    {
+      question:
+        "How can I report a bug or ask for help related to the backend?",
+      answer:
+        "If you encounter any issues or have questions regarding the backend setup, deployment, or any special feature implementation, you can contact the developer via email. In case you find a bug, you can submit an issue on the project's GitHub repository. Additionally, you are welcome to contribute by submitting pull requests with bug fixes or changes to the dev branch.",
+    },
+  ];
+
+  const [answerVisibility, setAnswerVisibility] = useState(
+    Array(faqs.length).fill(false),
+  );
+
+  return (
+    <>
+      <section className="pt-[75px] ">
+        <h3 className="text-center text-xl font-bold hidden md:block font-intercursive">
+          Frequently Asked Question&apos;s
+        </h3>
+        <h3 className="text-center text-xl font-bold block md:hidden font-intercursive">
+          FAQ&apos;s
+        </h3>
+        <center>
+          <div className="max-w-[1024px] font-interer px-[15px]">
+            <div className="max-w-[540px] w-[100%] ">
+              {faqs.map((value, index) => {
+                return (
+                  <section
+                    key={index}
+                    className="my-[16px] rounded-[4px] border-[1px] border-solid   "
+                  >
+                    <div className="overflow-hidden flex grid-rows-2 p-[13px] bg-[#fff] dark:bg-black rounded-[4px]">
+                      <button
+                        className="text-[14px] text-black dark:text-white  w-full text-left "
+                        onClick={() => {
+                          const newVisibility = [...answerVisibility];
+                          newVisibility[index] = !newVisibility[index];
+                          setAnswerVisibility(newVisibility);
+                        }}
+                      >
+                        {value.question}
+                      </button>
+                      <AiOutlineDownCircle
+                        className={`text-2xl text-right ${
+                          answerVisibility[index] ? "hidden" : "block"
+                        }`}
+                      />
+                      <AiOutlineUpCircle
+                        className={`text-2xl text-right  ${
+                          answerVisibility[index] ? "block" : "hidden"
+                        }`}
+                      />
+                    </div>
+                    <div
+                      className={`text-justify px-[13px] bg-[OEOEOE] overflow-hidden transition-max-height duration-200 ease-out text-[14px] text-[#1a1a1a] dark:text-white py-[17px] ${
+                        answerVisibility[index] ? "" : "hidden"
+                      }`}
+                    >
+                      <p>{value.answer}</p>
+                    </div>
+                  </section>
+                );
+              })}
+            </div>
+          </div>
+        </center>
+      </section>
+      <div className="bottom-0 md:hidden w-full font-interer pt-[10px]">
+        <hr />
+        <center>
+          <div className="flex justify-center mt-4 text-sm text-gray-600">
+            <a
+              href="https://github.com/thilakreddyy"
+              className="mx-2 hover:text-gray-900"
+            >
+              <FaGithub />
+            </a>
+            <a
+              href="https://twitter.com/thilakreddyonly"
+              className="mx-2 hover:text-gray-900"
+            >
+              <FaTwitter />
+            </a>
+            <a
+              href="https://www.instagram.com/__thilak_reddy__/"
+              className="mx-2 hover:text-gray-900"
+            >
+              <FaInstagram />
+            </a>
+          </div>
+          <div className="flex justify-center m-2 text-xs	 text-gray-600">
+            <p>&copy; 2023 jntuhresults.vercel.app</p>
+          </div>
+        </center>
+      </div>
+    </>
+  );
+};
+
+export default Faq;
--- app/helpcenter/page.tsx
+++ app/helpcenter/page.tsx
@@ -1,4 +1,79 @@
+import Link from "next/link";
+
 const HelpCenter = () => {
-  return <div>Help center</div>;
+  return (
+    <>
+      <div className="flex justify-center  mt-[6%]  mx-[16%] px-10 rounded-md   ">
+        <div className=" pt-[30px] pb-[50px]">
+          <div className=" md:text-2xl  font-semibold   flex justify-center ">
+            Help Center
+          </div>
+        </div>
+      </div>
+      <div className="text-center flex items-center justify-center">
+        <div className="home-links flex flex-wrap items-center justify-around max-w-4xl md:mt-6  sm:w-full">
+          <Link href="/faq">
+            <div className="border  hover:drop-shadow-sm group text-black  dark:text-white border-slate-800 md:border-gray-100 shadow-2xl max-w-xs p-6 mt-6 text-left md:w-96 rounded-xl hover:border-gray-500 transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-105 hover:bg-blue-300 duration-300">
+              <h3 className="group-hover:text-black text-lg sm:text-2xl font-bold">
+                <div className="flex flex-row items-center justify-start">
+                  <span className="p-1 text-black dark:text-white">
+                    Frequent questions
+                  </span>
+                  <svg
+                    stroke="currentColor"
+                    fill="currentColor"
+                    strokeWidth="0"
+                    viewBox="0 0 20 20"
+                    height="1em"
+                    width="1em"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                      clipRule="evenodd"
+                    ></path>
+                  </svg>
+                </div>
+              </h3>
+              <p className="group-hover:text-black text-slate-500 mt-4 text-base sm:text-xl">
+                The most Frequent asked questions are here!!!
+              </p>
+            </div>
+          </Link>
+          <Link href="https://docs.google.com/forms/d/e/1FAIpQLScFdsBs-QvzuZLxc1ZmvsUo4R2Ez1NPe0UmG7E1tgzXzKrimg/viewform">
+            <div className="border  hover:drop-shadow-sm group text-black  dark:text-white border-slate-800 md:border-gray-100 shadow-2xl max-w-xs p-6 mt-6 text-left md:w-96 rounded-xl hover:border-gray-500 transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-105 hover:bg-blue-300 duration-300">
+              <h3 className="group-hover:text-black text-lg sm:text-2xl font-bold">
+                <div className="flex flex-row items-center justify-start">
+                  <span className="p-1 text-black dark:text-white">
+                    Suggestion/ Feedback
+                  </span>
+                  <svg
+                    stroke="currentColor"
+                    fill="currentColor"
+                    strokeWidth="0"
+                    viewBox="0 0 20 20"
+                    height="1em"
+                    width="1em"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                      clipRule="evenodd"
+                    ></path>
+                  </svg>
+                </div>
+              </h3>
+              <p className="group-hover:text-black text-slate-500 mt-4 text-base sm:text-xl">
+                Share us the feedback or suggestion to help us in enhancing your
+                experience
+              </p>
+            </div>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
 };
 export default HelpCenter;
