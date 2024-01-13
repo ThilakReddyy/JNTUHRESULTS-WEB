abd1dd85029b3ce615f5215f6e03f9ab9147eca4
thilakreddyy <thilakreddy19102000@gmail.com>
2024-01-13 13:11:49 +0530

    change:defaultlight

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="light"
           enableSystem
           disableTransitionOnChange
         >
