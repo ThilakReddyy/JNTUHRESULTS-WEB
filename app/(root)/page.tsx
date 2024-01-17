3e82f1535b724b82e2b6dff6b10736b7fb4dbd6b
thilakreddyy <thilakreddy19102000@gmail.com>
2024-01-17 12:57:06 +0530

    Add:carrer opportunities

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -18,8 +18,6 @@ export default function Home() {
         await sleep(50);
       }
       while (true) {
-
-
         for (let i = 0; i < 5; i++) {
           setblind(true);
           await sleep(400);
@@ -27,7 +25,6 @@ export default function Home() {
           await sleep(400);
         }
       }
-
     };
 
     typeWriter();
@@ -105,6 +102,34 @@ export default function Home() {
               <p className="group-hover:text-black text-slate-500 mt-4 text-base sm:text-xl">
                 Compare your academic performance across all semesters with your
                 classmate.
+              </p>
+            </div>
+          </Link>
+          <Link href="/carrers">
+            <div className="border border-slate-800 md:border-gray-100 md:hidden hover:drop-shadow-sm group text-black dark:text-white shadow-2xl max-w-xs p-6 mt-6 text-left md:w-96 rounded-xl hover:border-gray-500 transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-105 hover:bg-blue-300 duration-300">
+              <h3 className="group-hover:text-black text-lg sm:text-2xl font-bold">
+                <div className="flex flex-row items-center justify-start">
+                  <span className="p-1 ">Jobs and Careers</span>
+                  <svg
+                    stroke="currentColor"
+                    fill="currentColor"
+                    strokeWidth="0"
+                    viewBox="0 0 20 20"
+                    height="1em"
+                    width="1em"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                      clipRule="evenodd"
+                    ></path>
+                  </svg>
+                </div>
+              </h3>
+              <p className="group-hover:text-black text-slate-500 mt-4 text-base sm:text-xl">
+                Discover career opportunities!! Find internships, jobs and
+                kickstart your professional journey.
               </p>
             </div>
           </Link>
