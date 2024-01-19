fdc9021c4c6749ec53b54e5133de9e7d50a1d389
thilakreddyy <thilakreddy19102000@gmail.com>
2024-01-19 13:16:49 +0530

    metadata

--- components/metadata/metadata.tsx
+++ components/metadata/metadata.tsx
@@ -14,15 +14,19 @@ const MetaData = () => {
         property="og:image"
         content="https://raw.githubusercontent.com/ThilakReddyy/JNTUHRESULTS-WEB/main/public/FrontPage.png"
       />
+      <meta
+        property="keywords"
+        content="jntuh Results, jntuh vercel, jntuh results vercel, JNTUH B.Tech Results, jntuhresults, jntuh notifications, JNTUH Results Engineering, JNTUH Engineering Results, jntuh bpharmacy results, jntuh bphar results, jntuh mtech results, jntuh mba results, jntuh mca results, jntuh all semester results, jntuh all semester results r18"
+      />
       <meta name="twitter:card" content="summary" />
       <meta property="twitter:title" content="JNTUH Results" />
       <meta
         property="twitter:description"
-        content="JNTUH Results, JNTUH B.Tech Results, jntuhresults, jntuh notifications, JNTUH Results Engineering, JNTUH Engineering Results, jntuh bpharmacy results, jntuh bphar results, jntuh mtech results, jntuh mba results, jntuh mca results, jntuh all semester results, jntuh all semester results r18"
+        content="JNTUH Results, JNTUH results vercel, jntuh vercel, JNTUH B.Tech Results, jntuhresults, jntuh notifications, JNTUH Results Engineering, JNTUH Engineering Results, jntuh bpharmacy results, jntuh bphar results, jntuh mtech results, jntuh mba results, jntuh mca results, jntuh all semester results, jntuh all semester results r18"
       />
       <meta
         name="description"
-        content="Find the results of your entire semesters at Jawaharlal Nehru Technological University, Hyderabad (JNTUH). Get JNTUH B.Tech results, notifications, and more."
+        content="Easily access your JNTUH results for {relevant course and semester} - Find out your grades, CGPA, backlogs, Jobs, Internships and more in one place. Check now!"
       />
       <meta
         name="google-site-verification"
