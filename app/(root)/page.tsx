40d79dd7f56375d245936ebf5e5ce4fbb5ffbdd0
thilakreddyy <thilakreddy19102000@gmail.com>
2024-01-31 22:01:10 +0530

    Add:credit checker

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -76,12 +76,11 @@ export default function Home() {
               </p>
             </div>
           </Link>
-
-          <Link href="/resultcontrast">
-            <div className="border border-slate-800 md:border-gray-100  hover:drop-shadow-sm group text-black dark:text-white shadow-2xl md:min-h-[220px]  max-w-xs p-6 mt-6 text-left md:w-96 rounded-xl hover:border-gray-500 transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-105 hover:bg-blue-300 duration-300">
+          <Link href="/creditchecker" className="md:hidden">
+            <div className="border border-slate-800 md:border-gray-100  hover:drop-shadow-sm group text-black  dark:text-white shadow-2xl max-w-xs p-6 mt-6 text-left md:w-96 rounded-xl hover:border-gray-500 transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-105 hover:bg-blue-300 duration-300">
               <h3 className="group-hover:text-black text-lg sm:text-2xl font-bold">
                 <div className="flex flex-row items-center justify-start">
-                  <span className="p-1">Results Contrast</span>
+                  <span className="p-1">Credit Checker</span>
                   <svg
                     stroke="currentColor"
                     fill="currentColor"
@@ -100,11 +99,40 @@ export default function Home() {
                 </div>
               </h3>
               <p className="group-hover:text-black text-slate-500 mt-4 text-base sm:text-xl">
-                Compare your academic performance across all semesters with your
-                classmate.
+                Check your credits to find out how many you need to move on to
+                the next year or to graduate.
               </p>
             </div>
           </Link>
+
+          <Link href="/backlogreport" className="md:hidden">
+            <div className="border border-slate-800 md:border-gray-100  hover:drop-shadow-sm group text-black  dark:text-white shadow-2xl max-w-xs p-6 mt-6 text-left md:w-96 rounded-xl hover:border-gray-500 transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-105 hover:bg-blue-300 duration-300">
+              <h3 className="group-hover:text-black text-lg sm:text-2xl font-bold">
+                <div className="flex flex-row items-center justify-start">
+                  <span className="p-1">Backlog Report</span>
+                  <svg
+                    stroke="currentColor"
+                    fill="currentColor"
+                    strokeWidth="0"
+                    viewBox="0 0 20 20"
+                    height="1em"
+                    width="1em"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                      clipRule="evenodd"
+                    ></path>
+                  </svg>
+                </div>
+              </h3>
+              <p className="group-hover:text-black text-slate-500 mt-4 text-base sm:text-xl">
+                Access your overall backlogs report with an hallticket.
+              </p>
+            </div>
+          </Link>
+
           <Link href="/carrers" className="md:hidden">
             <div className="border border-slate-800 md:border-gray-100 md:hidden hover:drop-shadow-sm group text-black dark:text-white shadow-2xl max-w-xs p-6 mt-6 text-left md:w-96 rounded-xl hover:border-gray-500 transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-105 hover:bg-blue-300 duration-300">
               <h3 className="group-hover:text-black text-lg sm:text-2xl font-bold">
@@ -130,6 +158,34 @@ export default function Home() {
               <p className="group-hover:text-black text-slate-500 mt-4 text-base sm:text-xl">
                 Discover career opportunities!! Find internships, jobs and
                 kickstart your professional journey.
+              </p>
+            </div>
+          </Link>
+          <Link href="/resultcontrast">
+            <div className="border border-slate-800 md:border-gray-100  hover:drop-shadow-sm group text-black dark:text-white shadow-2xl md:min-h-[220px]  max-w-xs p-6 mt-6 text-left md:w-96 rounded-xl hover:border-gray-500 transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-105 hover:bg-blue-300 duration-300">
+              <h3 className="group-hover:text-black text-lg sm:text-2xl font-bold">
+                <div className="flex flex-row items-center justify-start">
+                  <span className="p-1">Results Contrast</span>
+                  <svg
+                    stroke="currentColor"
+                    fill="currentColor"
+                    strokeWidth="0"
+                    viewBox="0 0 20 20"
+                    height="1em"
+                    width="1em"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                      clipRule="evenodd"
+                    ></path>
+                  </svg>
+                </div>
+              </h3>
+              <p className="group-hover:text-black text-slate-500 mt-4 text-base sm:text-xl">
+                Compare your academic performance across all semesters with your
+                classmate.
               </p>
             </div>
           </Link>
@@ -163,33 +219,6 @@ export default function Home() {
             </div>
           </Link>
 
-          <Link href="/backlogreport" className="md:hidden">
-            <div className="border border-slate-800 md:border-gray-100  hover:drop-shadow-sm group text-black  dark:text-white shadow-2xl max-w-xs p-6 mt-6 text-left md:w-96 rounded-xl hover:border-gray-500 transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-105 hover:bg-blue-300 duration-300">
-              <h3 className="group-hover:text-black text-lg sm:text-2xl font-bold">
-                <div className="flex flex-row items-center justify-start">
-                  <span className="p-1">Backlog Report</span>
-                  <svg
-                    stroke="currentColor"
-                    fill="currentColor"
-                    strokeWidth="0"
-                    viewBox="0 0 20 20"
-                    height="1em"
-                    width="1em"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                      clipRule="evenodd"
-                    ></path>
-                  </svg>
-                </div>
-              </h3>
-              <p className="group-hover:text-black text-slate-500 mt-4 text-base sm:text-xl">
-                Access your overall backlogs report with an hallticket.
-              </p>
-            </div>
-          </Link>
           <Link href="/notifications" className="md:hidden">
             <div className="border border-slate-800 md:border-gray-100  hover:drop-shadow-sm group text-black dark:text-white shadow-2xl max-w-xs p-6 mt-6 text-left md:w-96 rounded-xl hover:border-gray-500 transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-105 hover:bg-blue-300 duration-300">
               <h3 className="group-hover:text-black text-lg sm:text-2xl font-bold">
