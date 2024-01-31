5a459288adb25cf6ea01b2df5f5e5282c6261ba5
thilakreddyy <thilakreddy19102000@gmail.com>
2024-01-31 11:19:16 +0530

    Navitems

--- constants/navitems.tsx
+++ constants/navitems.tsx
@@ -99,6 +99,29 @@ export const navitems = [
     title: "Class Result",
   },
   {
+    href: "/creditchecker",
+    image: (
+      <>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="20"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="1.25"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="lucide lucide-credit-card"
+        >
+          <rect width="20" height="14" x="2" y="5" rx="2" />
+          <line x1="2" x2="22" y1="10" y2="10" />
+        </svg>
+      </>
+    ),
+    title: "Credits Checker",
+  },
+  {
     href: "/resultcontrast",
     image: (
       <>
