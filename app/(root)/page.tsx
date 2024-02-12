582386d76d136deabd3551214e9b4bf6b660c343
thilakreddyy <thilakreddy19102000@gmail.com>
2024-02-12 20:03:34 +0530

    Hide:dwnload app

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -153,7 +153,7 @@ export default function Home() {
           </Link>
         </div>
       </div>
-      <div className=" fixed bottom-0 my-5 flex w-full justify-center">
+      <div className="hidden fixed bottom-0 my-5  w-full justify-center">
         <Pwa />
       </div>
       <div className="bottom-0 xl:absolute w-full xl:pr-64 font-interer pt-[10px]">
