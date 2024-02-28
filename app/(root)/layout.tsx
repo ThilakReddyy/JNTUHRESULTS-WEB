502e1285a99fe7c236ac0fa4b81c930c210e1e85
thilakreddyy <thilakreddy19102000@gmail.com>
2024-02-28 12:33:37 +0530

    Add:api

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next";
 export const metadata: Metadata = {
   title: "JNTUH RESULTS - Check Your UG & PG Exam Results Online",
   description:
-    "Easily access your JNTUH results for {relevant course and semester} - Find out your grades, CGPA, backlogs, Jobs, Internships and more in one place. Check now!",
+    "Easily access your JNTUH results for relevant course and semester - Find out your grades, CGPA, backlogs, Jobs, Internships and more in one place. Check now!",
 };
 
 export default function RootLayout({
