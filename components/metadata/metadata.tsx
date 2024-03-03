90b3a17aac30d901b8db7e1766282ec9e8147cb3
thilakreddyy <thilakreddy19102000@gmail.com>
2024-03-04 04:22:22 +0530

    Add:metadata

--- components/metadata/metadata.tsx
+++ components/metadata/metadata.tsx
@@ -21,7 +21,7 @@ const MetaData = () => {
       />
       <meta
         property="keywords"
-        content="jntuh Results, jntuh vercel, vercel jntuh, jntuh results vercel, JNTUH B.Tech Results, jntuhresults, jntuh notifications, JNTUH Results Engineering, JNTUH Engineering Results, jntuh bpharmacy results, jntuh bphar results, jntuh mtech results, jntuh mba results, jntuh mca results, jntuh all semester results, jntuh all semester results r18"
+        content="jntuh, jntuh Results, jntuh vercel, vercel jntuh, jntuh results vercel,  jntuhresults, jntuh notifications, JNTUH Results Engineering, JNTUH Engineering Results, jntuh bpharmacy results, jntuh bphar results, jntuh mtech results, jntuh mba results, jntuh mca results, jntuh all semester results"
       />
       <meta name="publisher" content="Thilak Reddy" />
       <meta name="creator" content="Thilak Reddy" />
@@ -33,7 +33,7 @@ const MetaData = () => {
       />
       <meta
         property="twitter:description"
-        content="JNTUH Results, JNTUH results vercel, jntuh vercel, vercel jntuh, verceljntuh, JNTUH B.Tech Results, jntuhresults, jntuh notifications, JNTUH Results Engineering, JNTUH Engineering Results, jntuh bpharmacy results, jntuh bphar results, jntuh mtech results, jntuh mba results, jntuh mca results, jntuh all semester results, jntuh all semester results r18"
+        content="JNTUH Results, JNTUH results vercel, jntuh vercel, vercel jntuh, verceljntuh, jntuhresults, jntuh notifications,  jntuh bpharmacy results, jntuh bphar results, jntuh mtech results, jntuh mba results, jntuh mca results, jntuh all semester results, jntuh all semester results r18"
       />
       {/* <meta */}
       {/*   name="description" */}
