71decb2e57cd5029f04ec92575732f1f71cebc97
thilakreddyy <thilakreddy19102000@gmail.com>
2024-03-04 04:27:58 +0530

    change:metadata

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next";
 export const metadata: Metadata = {
   title: "JNTUH RESULTS",
   description:
-    "Get your JNTUH exam results for UG & PG courses effortlessly. Access your grades, CGPA, identify backlogs, and explore job & internship opportunities - all on one platform. Check your results now!",
+    "Get your JNTUH exam results (UG & PG) online easily! Get your grades, CGPA, and check for backlogs on the official JNTUH Vercel portal. Find jobs & internships too! Start now!",
 };
 
 export default function RootLayout({
