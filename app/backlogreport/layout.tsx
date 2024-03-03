f6158dc0b529cbeb207e74ebd5caf28f016cc79f
thilakreddyy <thilakreddy19102000@gmail.com>
2024-03-04 04:07:17 +0530

    Jntuh seo

--- app/backlogreport/layout.tsx
+++ app/backlogreport/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "JNTUH RESULTS | BACKLOG ASSESMENT",
-  description: "Check out backlogs result with in a go.",
+  description:
+    "Get a clear picture of your JNTUH academic standing with our easy-to-use backlog assessment tool. See your current backlog status and plan your next steps efficiently.",
 };
 
 export default function RootLayout({
