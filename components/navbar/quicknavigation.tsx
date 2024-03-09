de038394eb7717377fd9ef50013871b4906298ae
thilakreddyy <thilakreddy19102000@gmail.com>
2024-03-09 15:10:45 +0530

    Fix : quick navigation

--- components/navbar/quicknavigation.tsx
+++ components/navbar/quicknavigation.tsx
@@ -10,7 +10,7 @@ const QuickNavigation = ({ htno }: { htno: string | null }) => {
   }
   return (
     <>
-      <div className="fixed bottom-0  right-0 m-5 rounded-full bg-black dark:bg-white dark:text-black  text-white border p-2 z-[401] ">
+      <div className="fixed bottom-0   right-0 m-5 cursor-pointer rounded-full bg-black dark:bg-white dark:text-black  text-white border p-2 z-[401] ">
         <GrAdd
           className={`${extraItems ? "hidden" : ""}`}
           onClick={() => {
@@ -25,28 +25,28 @@ const QuickNavigation = ({ htno }: { htno: string | null }) => {
         />
       </div>
       <div
-        className={`${extraItems ? "fixed w-[100%] h-[100%] bg-opacity-50 backdrop-filter  backdrop-blur-sm bottom-0 z-[400] top-[64px] text-white" : ""}`}
+        className={`${extraItems ? "fixed w-[100%]  h-[100%] bg-opacity-50 backdrop-filter  backdrop-blur-sm bottom-0 z-[400] top-[64px] text-white" : ""}`}
       ></div>
 
       <div
-        className={`${extraItems ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"} transition-opacity z-[401] duration-300 fixed bottom-10 right-2 m-5 rounded bg-white text-black text-[12px] border p-2 w-[80%]`}
+        className={`${extraItems ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"} transition-opacity z-[401] duration-300 fixed bottom-10 right-2 m-5 rounded bg-white text-black text-[12px] border p-2 `}
       >
-        <div className="flex flex-col justify-center">
+        <div className="flex flex-col justify-center ">
           <Link
             href={`/academicresult/result?htno=${htno}`}
-            className="py-2 border  border-b-0 border-t-rounded flex rounded-t justify-center  text-blue-600 cursor-pointer"
+            className="py-2 border  border-b-0 border-t-rounded flex rounded-t px-20  justify-center  text-blue-600 cursor-pointer"
           >
             Academic Result
           </Link>
           <Link
             href={`/backlogreport/result?htno=${htno}`}
-            className="py-2 border px-2 flex justify-center  text-blue-600 cursor-pointer"
+            className="py-2 border px-20 flex justify-center  text-blue-600 cursor-pointer"
           >
             Backlog Report
           </Link>
           <Link
             href={`/creditchecker/result?htno=${htno}`}
-            className="p-2 border border-t-0 flex justify-center rounded-b  text-blue-600 cursor-pointer"
+            className="p-2 border border-t-0 px-20 flex justify-center rounded-b  text-blue-600 cursor-pointer"
           >
             Credits Checker
           </Link>
