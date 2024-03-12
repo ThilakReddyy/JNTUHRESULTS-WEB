5e13f753f90adf60e64b392c4ce25c4e73b7350b
thilakreddyy <thilakreddy19102000@gmail.com>
2024-03-12 21:16:50 +0530

    Fix:academic result

--- components/api/fetchAcademicResult.tsx
+++ components/api/fetchAcademicResult.tsx
@@ -85,7 +85,6 @@ export const getLocalStoragedata = (htno: string, backlog: boolean = false) => {
     return null;
   }
 };
-const sleep = (ms: any) => new Promise((resolve) => setTimeout(resolve, ms));
 
 export async function fetchAcademicResult(htno: string) {
   //Redis Data
@@ -101,6 +100,7 @@ export async function fetchAcademicResult(htno: string) {
     "https://jntuhresultss.vercel.app/api/academicresult?htno=",
     "https://jntuhresultsss.vercel.app/api/academicresult?htno=",
   ];
+  console.log("came here");
   const primaryUrl = urls[3] + htno;
   // const primaryUrl = `https://jntuhresults.up.railway.app/api/academicresult?htno=${htno}`;
   //const primaryUrl = `/api/academicresult?htno=${htno}`;
