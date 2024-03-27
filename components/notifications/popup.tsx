66afbb48afecc248caa05f344be04462a0ce32d1
thilakreddyy <thilakreddy19102000@gmail.com>
2024-03-27 07:13:29 +0530

    logical popup

--- components/notifications/popup.tsx
+++ components/notifications/popup.tsx
@@ -8,6 +8,7 @@ import { usePathname } from "next/navigation";
 import { Button } from "../ui/button";
 import { NotificationList } from "@/constants/notifications";
 import { FaTelegram } from "react-icons/fa";
+import NoticePopup from "../homepage/notice";
 
 const NotificationPopUp = () => {
   const [results, setResults] = useState<Result[]>(NotificationList);
@@ -54,82 +55,94 @@ const NotificationPopUp = () => {
   }, []);
 
   return (
-    <div
-      className={`lg:hidden bg-opacity-50 backdrop-filter  backdrop-blur-sm fixed h-full   my-5  w-full  justify-center ${path !== "/" || hidden || results.length === 0 ? "hidden" : ""}`}
-    >
-      <div className="flex justify-center items-center h-full">
+    <>
+      {results.length !== 0 ? (
         <div
-          className={`md:w-[50%] font-bold text-center flex justify-center dark:bg-[#1B1C1E] w-full  items-center shadow-xl bg-white p-2 rounded-md border m-2 `}
+          className={`lg:hidden bg-opacity-50 backdrop-filter  backdrop-blur-sm fixed h-full   my-5  w-full  justify-center ${path !== "/" || hidden || results.length === 0 ? "hidden" : ""}`}
         >
-          <div className="text-center">
-            <div className="py-2 flex justify-around ">
-              <div></div>
-              <div>Results have been Released!!!</div>
-              <div className="flex items-center">
-                <Button
-                  onClick={() => {
-                    setHidden(true);
-                  }}
-                  className="h-[2px] rounded w-[2px] px-[10px] text-[10px]"
-                >
-                  X
-                </Button>
-              </div>
-            </div>
-            <div className=" h-[412px] p-2 overflow-auto">
-              {results.map((result: any, index: number) => {
-                return (
-                  <div
-                    key={index}
-                    className={`border px-1 py-2 ${index === 0 ? "" : "border-t-0"}`}
-                  >
-                    <div className="font-sans text-sm ">
-                      {result["Result_title"]}
-                    </div>
-                    <table className="dark:border-white">
-                      <tbody className="text-xs md:text-lg">
-                        <tr>
-                          <th className="dark:border-white">Result Link 1</th>
-                          <th className="dark:border-white">
-                            <Link
-                              href={`http://202.63.105.184/results/jsp/SearchResult.jsp?${result["Link"].split("?")[1]}`}
-                              target="_blank"
-                              className="text-blue-500 hover:underline cursor-pointer"
-                            >
-                              link
-                            </Link>
-                          </th>
-                        </tr>
-                        <tr>
-                          <th className="dark:border-white">Result Link 2</th>
-                          <th className="dark:border-white">
-                            <Link
-                              href={`http://results.jntuh.ac.in/results/jsp/SearchResult.jsp?${result["Link"].split("?")[1]}`}
-                              target="_blank"
-                              className="text-blue-500 hover:underline cursor-pointer"
-                            >
-                              link
-                            </Link>
-                          </th>
-                        </tr>
-                      </tbody>
-                    </table>
+          <div className="flex justify-center items-center h-full">
+            <div
+              className={`md:w-[50%] font-bold text-center flex justify-center dark:bg-[#1B1C1E] w-full  items-center shadow-xl bg-white p-2 rounded-md border m-2 `}
+            >
+              <div className="text-center">
+                <div className="py-2 flex justify-around ">
+                  <div></div>
+                  <div>Results have been Released!!!</div>
+                  <div className="flex items-center">
+                    <Button
+                      onClick={() => {
+                        setHidden(true);
+                      }}
+                      className="h-[2px] rounded w-[2px] px-[10px] text-[10px]"
+                    >
+                      X
+                    </Button>
                   </div>
-                );
-              })}
-              <Link
-                href="https://t.me/jntuhvercel"
-                target="_blank"
-                className="flex dark:bg-white p-2 bg-black text-white dark:text-black justify-center items-center mt-4"
-              >
-                Join us on Telegram{"  "}
-                <FaTelegram size={18} className="ml-1" />
-              </Link>
+                </div>
+                <div className=" h-[412px] p-2 overflow-auto">
+                  {results.map((result: any, index: number) => {
+                    return (
+                      <div
+                        key={index}
+                        className={`border px-1 py-2 ${index === 0 ? "" : "border-t-0"}`}
+                      >
+                        <div className="font-sans text-sm ">
+                          {result["Result_title"]}
+                        </div>
+                        <table className="dark:border-white">
+                          <tbody className="text-xs md:text-lg">
+                            <tr>
+                              <th className="dark:border-white">
+                                Result Link 1
+                              </th>
+                              <th className="dark:border-white">
+                                <Link
+                                  href={`http://202.63.105.184/results/jsp/SearchResult.jsp?${result["Link"].split("?")[1]}`}
+                                  target="_blank"
+                                  className="text-blue-500 hover:underline cursor-pointer"
+                                >
+                                  link
+                                </Link>
+                              </th>
+                            </tr>
+                            <tr>
+                              <th className="dark:border-white">
+                                Result Link 2
+                              </th>
+                              <th className="dark:border-white">
+                                <Link
+                                  href={`http://results.jntuh.ac.in/results/jsp/SearchResult.jsp?${result["Link"].split("?")[1]}`}
+                                  target="_blank"
+                                  className="text-blue-500 hover:underline cursor-pointer"
+                                >
+                                  link
+                                </Link>
+                              </th>
+                            </tr>
+                          </tbody>
+                        </table>
+                      </div>
+                    );
+                  })}
+                  <div>
+                    <Link
+                      href="https://t.me/jntuhvercel"
+                      target="_blank"
+                      className="flex dark:bg-white p-2 bg-black text-white dark:text-black justify-center items-center mt-4"
+                    >
+                      Join us on Telegram{"  "}
+                      <FaTelegram size={18} className="ml-1" />
+                    </Link>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </div>
+      ) : (
+        <NoticePopup />
+      )}
+    </>
   );
 };
 export default NotificationPopUp;
