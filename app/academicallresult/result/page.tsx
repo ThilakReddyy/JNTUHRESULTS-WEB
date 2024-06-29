39ce897ab72f83625fab7237ae4848f5b30f7359
thilakreddy <thilakreddy19102000@gmail.com>
2024-06-29 08:16:17 +0530

    Fix RCRV issue

--- app/academicallresult/result/page.tsx
+++ app/academicallresult/result/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useSearchParams, useRouter } from "next/navigation";
 import Print from "@/components/download/print";
 import ResultDetails from "@/components/result/details";
@@ -7,8 +7,6 @@ import ResultDetails from "@/components/result/details";
 const AcademicAllResultResult = () => {
   const router = useRouter();
   const htno = useSearchParams().get("htno");
-  console.log(htno);
-  console.log(String(htno) + "all");
 
   const storageData = localStorage.getItem(String(htno) + "all");
   const componentRef = useRef(null);
@@ -22,7 +20,6 @@ const AcademicAllResultResult = () => {
     details?.value?.data || {};
 
   const [renderKey] = useState(0);
-  console.log(resultdetails);
   const [Details] = useState(resultdetails);
   const [Results, setResults] = useState(resultresults);
   useEffect(() => {
@@ -65,77 +62,104 @@ const AcademicAllResultResult = () => {
                     </tr>
                   </tbody>
                 </table>
-                {Object.keys(Results[value]).map((examcode: string) => {
+                {Object.keys(Results[value]).map((examcode) => {
                   return (
-                    <div key={examcode}>
-                      <table className="dark:border-white dark:bg-gray-900">
-                        <tbody>
-                          <tr>
-                            <th className="bg-gray-200 md:bg-gray-300 dark:bg-[#0b3954] dark:border-white">
-                              Exam Code: {examcode}
-                            </th>
+                    <React.Fragment key={examcode}>
+                      {Results[value][examcode].map(
+                        (subexam: any, index: number) => {
+                          const exms = Object.keys(subexam)[0];
+                          const rcrv = subexam[exms]["subject_internal"]
+                            .toLowerCase()
+                            .includes("change in grade");
 
-                            <th className="bg-gray-200 md:bg-gray-300 dark:bg-[#0b3954] dark:border-white">
-                              <a
-                                href={`http://results.jntuh.ac.in/results/resultAction?degree=${htno?.[5] === "R" ? "bpharmacy" : "btech"}&examCode=${examcode}&etype=r16&result=null&grad=null&type=intgrade&htno=${htno}`}
-                                target="_blank"
-                                className="underline"
-                              >
-                                Direct Link
-                              </a>
-                            </th>
-                          </tr>
-                        </tbody>
-                      </table>
-                      <table className="dark:border-white dark:bg-gray-900">
-                        <tbody>
-                          <tr className="w-max bg-gray-200 md:bg-gray-300 dark:border-white dark:bg-[#0b3954]">
-                            <th className="dark:border-white px-1">
-                              SUBJECT_CODE
-                            </th>
-                            <th className="dark:border-white px-1">
-                              SUBJECT_NAME
-                            </th>
-                            <th className="dark:border-white px-1">INTERNAL</th>
-                            <th className="dark:border-white px-1">EXTERNAL</th>
-                            <th className="dark:border-white px-1">TOTAL</th>
-                            <th className="dark:border-white px-1">GRADE</th>
-                            <th className="dark:border-white px-1">CREDITS</th>
-                          </tr>
-                          {Object.values(Results[value][examcode]).map(
-                            (subjectvalue: any, index: number) => {
-                              return (
-                                <tr key={index}>
-                                  <th className="dark:border-white">
-                                    {subjectvalue["subject_code"]}
-                                  </th>
-                                  <th className="dark:border-white">
-                                    {subjectvalue["subject_name"]}
-                                  </th>
-                                  <th className="dark:border-white">
-                                    {subjectvalue["subject_internal"]}
-                                  </th>
-                                  <th className="dark:border-white">
-                                    {subjectvalue["subject_external"]}
-                                  </th>
-                                  <th className="dark:border-white">
-                                    {subjectvalue["subject_total"]}
-                                  </th>
-                                  <th
-                                    className={`dark:border-white ${["f", "ab", "-"].includes(subjectvalue["subject_grade"].toLowerCase()) ? "text-red-500" : ""}`}
-                                  >
-                                    {subjectvalue["subject_grade"]}
-                                  </th>
-                                  <th className="dark:border-white">
-                                    {subjectvalue["subject_credits"]}
-                                  </th>
-                                </tr>
-                              );
-                            },
-                          )}
-                        </tbody>
-                      </table>
-                    </div>
+                          return (
+                            <div key={index}>
+                              <table className="dark:border-white dark:bg-gray-900">
+                                <tbody>
+                                  <tr>
+                                    <th className="bg-gray-200 md:bg-gray-300 dark:bg-[#0b3954] dark:border-white">
+                                      Exam Code: {examcode}
+                                    </th>
+                                    {rcrv && (
+                                      <th className="bg-gray-200 md:bg-gray-300 dark:bg-[#0b3954] dark:border-white">
+                                        Result Type: RC/RV
+                                      </th>
+                                    )}
+                                    <th className="bg-gray-200 md:bg-gray-300 dark:bg-[#0b3954] dark:border-white">
+                                      <a
+                                        href={`http://results.jntuh.ac.in/results/resultAction?degree=${htno?.[5] === "R" ? "bpharmacy" : "btech"}&examCode=${examcode}&etype=r16&result=${rcrv ? "gradercrv" : "null"}&grad=null&type=${rcrv ? "rcrvintgrade" : "intgrade"}&htno=${htno}`}
+                                        target="_blank"
+                                        className="underline"
+                                      >
+                                        Direct Link
+                                      </a>
+                                    </th>
+                                  </tr>
+                                </tbody>
+                              </table>
+                              <table className="dark:border-white dark:bg-gray-900">
+                                <tbody>
+                                  <tr className="w-max bg-gray-200 md:bg-gray-300 dark:border-white dark:bg-[#0b3954]">
+                                    <th className="dark:border-white px-1">
+                                      SUBJECT_CODE
+                                    </th>
+                                    <th className="dark:border-white px-1">
+                                      SUBJECT_NAME
+                                    </th>
+                                    <th className="dark:border-white px-1">
+                                      INTERNAL
+                                    </th>
+                                    <th className="dark:border-white px-1">
+                                      EXTERNAL
+                                    </th>
+                                    <th className="dark:border-white px-1">
+                                      TOTAL
+                                    </th>
+                                    <th className="dark:border-white px-1">
+                                      GRADE
+                                    </th>
+                                    <th className="dark:border-white px-1">
+                                      CREDITS
+                                    </th>
+                                  </tr>
+                                  {Object.values(subexam).map(
+                                    (subjectvalue: any, index: number) => {
+                                      return (
+                                        <tr key={index}>
+                                          <th className="dark:border-white">
+                                            {subjectvalue["subject_code"]}
+                                          </th>
+                                          <th className="dark:border-white">
+                                            {subjectvalue["subject_name"]}
+                                          </th>
+                                          <th className="dark:border-white">
+                                            {subjectvalue["subject_internal"]}
+                                          </th>
+                                          <th className="dark:border-white">
+                                            {subjectvalue["subject_external"]}
+                                          </th>
+                                          <th className="dark:border-white">
+                                            {subjectvalue["subject_total"]}
+                                          </th>
+                                          <th
+                                            className={`dark:border-white ${["f", "ab", "-"].includes(subjectvalue["subject_grade"].toLowerCase()) ? "text-red-500" : ""}`}
+                                          >
+                                            {subjectvalue["subject_grade"]}
+                                          </th>
+                                          <th className="dark:border-white">
+                                            {subjectvalue["subject_credits"]}
+                                          </th>
+                                        </tr>
+                                      );
+                                    },
+                                  )}
+                                </tbody>
+                              </table>
+                            </div>
+                          );
+                        },
+                      )}
+                    </React.Fragment>
                   );
                 })}
 
