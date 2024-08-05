5478cb2bf4547d11a01564b2eb5066444124e417
thilakreddyy <thilakreddy19102000@gmail.com>
2024-08-05 10:05:36 +0530

    Add:NavItem Calendar

--- constants/navitems.tsx
+++ constants/navitems.tsx
@@ -195,6 +195,31 @@ export const navitems = [
     },
   ],
   {
+    href: "/calendars",
+    image: (
+      <>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="20"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          className="lucide lucide-calendar"
+        >
+          <path d="M8 2v4" />
+          <path d="M16 2v4" />
+          <rect width="18" height="18" x="3" y="4" rx="2" />
+          <path d="M3 10h18" />
+        </svg>
+      </>
+    ),
+    title: "Calendars",
+  },
+  {
     href: "/syllabus",
     image: (
       <>
