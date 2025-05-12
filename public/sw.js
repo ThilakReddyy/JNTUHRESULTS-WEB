8a6e5ed0f6381c44084353724243620dccbf2ae9
thilakreddyy <thilakreddy19102000@gmail.com>
2025-05-12 15:34:55 +0530

    Add: url

--- public/sw.js
+++ public/sw.js
@@ -4,6 +4,7 @@ self.addEventListener("push", function (event) {
     self.registration.showNotification(data.title, {
       body: data.body,
       icon: "/android-chrome-512x512.png",
+      url: "https://jntuhresults.vercel.app/academicresult",
     }),
   );
 });
