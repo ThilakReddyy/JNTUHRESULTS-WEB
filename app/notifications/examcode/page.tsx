0ac23a98c8db9f3beddabde795c05ef2649582a3
thilakreddyy <thilakreddy19102000@gmail.com>
2026-02-04 15:25:51 +0530

    Remove: api calls

--- app/notifications/examcode/page.tsx
+++ app/notifications/examcode/page.tsx
@@ -1,35 +1,5 @@
+"use client"
 import NotificationExamCode from "@/components/notifications/notificationExamCode";
-
-export async function generateMetadata({
-  searchParams,
-}: {
-  searchParams: {
-    link?: string;
-    date?: string;
-    formatted_date?: string;
-    title?: string;
-  };
-}) {
-  const { title = "", formatted_date = "", date = "" } = searchParams;
-
-  return {
-    title: title ? `${title} | JNTUH RESULTS` : "JNTUH RESULTS | Notifications",
-    description: title
-      ? `Check ${title} released on ${formatted_date || date}.`
-      : "Check out JNTUH notifications in one place.",
-    openGraph: {
-      title: title || "JNTUH RESULTS",
-      description: `Exam Notification - ${title} (${formatted_date || date})`,
-      type: "website",
-      siteName: "JNTUH RESULTS",
-    },
-    twitter: {
-      card: "summary_large_image",
-      title: title || "JNTUH RESULTS",
-      description: `Exam Notification - ${title} (${formatted_date || date})`,
-    },
-  };
-}
 
 export default function ExamcodePage({
   searchParams,
