982ec1f5b0f2249996222379dc950fdbef09346d
thilakreddyy <thilakreddy19102000@gmail.com>
2026-02-04 15:36:01 +0530

    Add: next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
-};
+  output: "export",
+  images: {
+    unoptimized: true
+  }
+}
 
 module.exports = nextConfig;
