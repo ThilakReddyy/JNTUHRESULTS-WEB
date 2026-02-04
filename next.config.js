9c385714b3800d6fa117037d1dba3b4ccff9ae0c
thilakreddyy <thilakreddy19102000@gmail.com>
2026-02-04 15:53:25 +0530

    Fix: standalone to out

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true
-  }
+
+  output: 'export'
+
 }
 
 module.exports = nextConfig;
