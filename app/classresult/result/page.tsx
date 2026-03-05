e8acacb4f400cd811a709b7851a041dfc25dbf82
thilakreddyy <thilakreddy19102000@gmail.com>
2026-03-05 13:09:08 +0530

    Fix: auto zoom it

--- app/classresult/result/page.tsx
+++ app/classresult/result/page.tsx
@@ -28,7 +28,7 @@ const ClassResultResult = () => {
   return (
     <>
       <div
-        className="mx-auto px-3 pb-6 text-[30%] sm:text-[45%] md:text-[60%] lg:text-[100%]"
+        className="mx-auto px-3 pb-6"
         ref={componentRef}
       >
         {/* Page header */}
