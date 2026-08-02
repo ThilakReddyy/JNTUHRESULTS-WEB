f98263d3f70ece6949628dd7759f8d43675abc26
thilakreddy <thilakreddy19102000@gmail.com>
2026-08-03 03:04:43 +0530

    Default website theme to light

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="dark"
+          defaultTheme="light"
           enableSystem
           disableTransitionOnChange
         >
